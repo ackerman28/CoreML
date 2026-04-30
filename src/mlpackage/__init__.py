8a203390cb30d24d3b48c40df38023c2fae9b843
ackerman28 <heyitzavik@gmail.com>
2026-04-29 20:55:20 -0500

    feat: implement K-Nearest Neighbors with Euclidean distance and visualization

--- src/mlpackage/__init__.py
+++ src/mlpackage/__init__.py
@@ -9,3 +9,4 @@ from .supervised_learning.perceptron import Perceptron
 from .supervised_learning.linear_regression import LinearRegression
 from .supervised_learning.logistic_regression import LogisticRegression
 from .supervised_learning.mlp import MultilayerPerceptron
+from .supervised_learning.knn import KNN
--- src/mlpackage/supervised_learning/__init__.py
+++ src/mlpackage/supervised_learning/__init__.py
@@ -2,3 +2,4 @@ from .perceptron import Perceptron
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
 from .mlp import MultilayerPerceptron
+from .knn import KNN
